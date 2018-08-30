44f1a34686bf7f477aea97e8c70b7ae735c02d9b
Philip <philip.dissert@student.kit.edu>
2018-08-30 16:03:45 +0200

    initial commit

---
+++ src/main/java/edu/kit/cm/WorkspaceManagement/AtisAppBffApplication.java
@@ -0,0 +1,15 @@
+package edu.kit.cm.WorkspaceManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class AtisAppBffApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AtisAppBffApplication.class, args);
+	}
+
+}
